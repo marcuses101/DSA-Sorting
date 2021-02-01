7f7a9c9e107e46e6c10c1cf6611d3e1aae00b7ca
marcuses101 <mnjconnolly@gmail.com>
2021-02-01 10:45:37 -0500

    add merge sort

--- index.js
+++ index.js
@@ -18,54 +18,76 @@ function mergeSort(array) {
   }
 
   const middle = Math.floor(array.length / 2);
-  let left = mergeSort(array.slice(0,middle));
-  let right= mergeSort(array.slice(middle));
-  return merge(left,right,array)
+  let left = mergeSort(array.slice(0, middle));
+  let right = mergeSort(array.slice(middle));
+  return merge(left, right, array);
 }
 
-function merge(left,right,array){
+function merge(left, right, array) {
   let leftIndex = 0;
   let rightIndex = 0;
   let outputIndex = 0;
-  while (leftIndex < left.length && rightIndex < right.length){
-    if (left[leftIndex] < right[rightIndex]){
-      array[outputIndex++] = left[leftIndex++]
-    }
-    else {
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      array[outputIndex++] = left[leftIndex++];
+    } else {
       array[outputIndex++] = right[rightIndex++];
     }
   }
 
-  for (let i = leftIndex; i < left.length; i++){
+  for (let i = leftIndex; i < left.length; i++) {
     array[outputIndex++] = left[i];
   }
 
-  for (let i = rightIndex; i < right.length; i++){
+  for (let i = rightIndex; i < right.length; i++) {
     array[outputIndex++] = right[i];
   }
   return array;
 }
 
-function quickSort(array,start = 0, end = array.length){
-  if (start >= end){
+function quickSort(array, start = 0, end = array.length) {
+  if (start >= end) {
     return array;
   }
 
-  const middle = partition (array,start,end);
-  array = quickSort(array,start,middle);
-  array = quickSort(array,middle + 1, end);
+  const middle = partition(array, start, end);
+  array = quickSort(array, start, middle);
+  array = quickSort(array, middle + 1, end);
   return array;
 }
 
-function partition(array,start,end){
-  const pivot = array[end-1];
+function partition(array, start, end) {
+  const pivot = array[end - 1];
   let j = start;
-  for (let i = start; i < end -1; i++){
-    if (array[i] <=pivot){
-      swap(array,i,j);
+  for (let i = start; i < end - 1; i++) {
+    if (array[i] <= pivot) {
+      swap(array, i, j);
       j++;
     }
   }
-  swap(array,end-1,j);
+  swap(array, end - 1, j);
   return j;
 }
+
+function arrayAtRecursion(int, array) {
+  let sortCalls = 0;
+  let mergeCalls = 0;
+
+  function mergeSort(array) {
+    sortCalls++;
+    if ((sortCalls === int)) console.log(array);
+    if (array.length <= 1) {
+      return array;
+    }
+
+    const middle = Math.floor(array.length / 2);
+    let left = mergeSort(array.slice(0, middle));
+    let right = mergeSort(array.slice(middle));
+    return merge(left, right, array);
+  }
+  return mergeSort(array);
+}
+const array = [21, 1, 26, 45, 29, 28, 2, 9, 16, 49, 39, 27, 43, 34, 46, 40]
+console.log(quickSort(array));
+
+//
